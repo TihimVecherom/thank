bdc2023318f80172568a6b736c338e254efb348c
TihimVecherom <98401044+TihimVecherom@users.noreply.github.com>
2023-05-03 00:39:49 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,33 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    const menuBurger = document.querySelector(".menu__icon");
+    const menuBody = document.querySelector(".menu__body");
+    if (menuBurger) menuBurger.addEventListener("click", (function(e) {
+        menuBurger.classList.toggle("_icon-active");
+        if (menuBody) menuBody.classList.toggle("_menu-active");
+    }));
+    window["FLS"] = true;
+    isWebp();
+})();
